b7294751e0a6c8aa8eaed73723c8889a226c14cd
Vincent Rodley <vindognz@gmail.com>
2026-06-30 16:11:03 +1200

    Player and spike now drawn in code (collisions match), tuned movement!

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,15 +1,30 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = 400.0
+const SPEED = 80.0
+const JUMP_VELOCITY = 350.0
+const GRAVITY = Vector2(0, 1200) #Vector2(0, 980.0)
+
+func _draw():
+	var style = StyleBoxFlat.new()
+	style.bg_color = Color(1.0, 1.0, 1.0, 1.0) # your purple
+	style.corner_radius_top_left = 2
+	style.corner_radius_top_right = 2
+	style.corner_radius_bottom_left = 2
+	style.corner_radius_bottom_right = 2
+	style.border_width_left = 1
+	style.border_width_right = 1
+	style.border_width_top = 1
+	style.border_width_bottom = 1
+	style.border_color = Color(0.0, 0.0, 0.0, 1.0) # white
+	draw_style_box(style, Rect2(-8, -8, 16, 16))
 
 func _physics_process(delta: float) -> void:
 	# Handle gravity
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += GRAVITY * delta
 
 	# Handle jumping
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = -JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
--- scripts/spike.gd
+++ scripts/spike.gd
@@ -2,18 +2,40 @@ extends Node2D
 
 const SPEED = 100
 @onready var camera: Camera2D = get_parent().get_node("Camera")
+@onready var collision_main: CollisionPolygon2D = $Area2D/CollisionPolygon2D
+@onready var collision_secondary: CollisionPolygon2D = $Area2D/CollisionPolygon2D2
+
 var screen_width: float
 
+var main_points = PackedVector2Array([
+	Vector2(-8, 8),
+	Vector2(2, 8),
+	Vector2(-3, -8)
+])
+
+var secondary_points = PackedVector2Array([
+	Vector2(0, 8),
+	Vector2(8, 8),
+	Vector2(4, -3)
+])
+
+func _draw():
+	var color = Color(0.9, 0.1, 0.2)
+	draw_polygon(main_points, PackedColorArray([color]))
+	draw_polygon(secondary_points, PackedColorArray([color]))
+	
 # Called once upon instantiation
 func _ready() -> void:
 	screen_width = get_viewport_rect().size.x / camera.zoom.x
+	collision_main.polygon = main_points
+	collision_secondary.polygon = secondary_points
 
 # Called every frame.
 func _process(delta: float) -> void:
 	position.x += SPEED * delta
-	
 	if position.x > screen_width/2:
 		position.x = -screen_width/2
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("COLLISION")
+	if body is CharacterBody2D:
+		print("COLLISION")
